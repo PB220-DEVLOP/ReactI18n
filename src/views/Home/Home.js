d0d33bf708b7d4d92a308890f76e446fef2ec4bf
Dhanashree312 <dhanashree@gmail.com>
2023-11-26 20:32:48 +0530

    FIrst commit

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,31 @@
-import React from 'react'
-import './Home.css'
-
+import React from 'react';
+import './Home.css';
+import I18n from '../../utils/I18n';
 function Home(){
+
+    const users=15;
+
+
     return (
-   <div>Home</div>
+        <>
+  <h1>{I18n("welcomemessage")}</h1>
+
+  <p>{I18n("normalmessage")}</p>
+  
+  <h3>{I18n("greetingmessage")}</h3>
+<select 
+defaultValue={localStorage.getItem("lang")}
+onChange={(e)=>{
+    localStorage.setItem("lang",e.target.value);
+    window.location.reload();
+}}>
+    <option value="mr">Marathi</option>
+    <option value="hi">Hindi</option>
+    <option value="en">English</option>
+</select>
+
+  <p>{users} {I18n("Userscount")}</p>
+  </>
     )
 }
 
